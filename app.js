46281529421b21202aa44c15ac6389a99780988f
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-09-20 12:57:44 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//----Encabezado
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser')
+const port = process.env.PORT || 3000
+
+//----Express
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs') 
+app.use(express.static(__dirname + '/public'))
+app.listen(port, function() {
+    console.log('Enlazado a Puerto')
+})
+
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+  
+})
