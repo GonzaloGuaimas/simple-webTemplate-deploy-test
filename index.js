608ec1d39b0bb17b27182705be809b837f06e916
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-09-20 14:59:52 -0300

    refactor files

--- index.js
+++ index.js
@@ -10,20 +10,3 @@ app.get('/', (req, res) => {
 })
 // index.js
 module.exports = app
-
-/*
-const bodyParser= require('body-parser')
-const port = process.env.PORT || 3000
-
-//----Express
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs') 
-app.use(express.static(__dirname + '/public'))
-app.listen(port, function() {
-    console.log('Enlazado a Puerto')
-})
-
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
-*/
